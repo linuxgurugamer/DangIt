531c9dbb75d47806ec1ba7db9706ff0f4ab7c7ab
Jonathan Bayer <linuxgurugamer@gmail.com>
2022-11-29 20:55:17 -0500

    meh

--- DangIt/AssemblyVersion.cs
+++ DangIt/AssemblyVersion.cs
@@ -5,5 +5,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("0.7.20.6")]
- [assembly: AssemblyFileVersion("0.7.20.6")]
+ [assembly: AssemblyVersion("0.7.20.7")]
+ [assembly: AssemblyFileVersion("0.7.20.7")]
